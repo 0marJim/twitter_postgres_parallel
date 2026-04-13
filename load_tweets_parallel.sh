3e4457992afc174846aa76c453008344b56cd9f6
0marJim <omar.jimenez.us@gmail.com>
2026-04-12 22:26:35 -0700

    working

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,18 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 files=$(find data/*)
 
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+# The quotes are tricky here, so we carefully escape the \ characters for the COPY command
+time echo "$files" | parallel "unzip -p {} | sed 's/\\\\u0000//g' | psql postgresql://postgres:pass@localhost:20001/postgres -c \"COPY tweets_jsonb (data) FROM STDIN csv quote e'\\x01' delimiter e'\\x02';\""
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+time echo "$files" | parallel 'python3 load_tweets.py --db=postgresql://postgres:pass@localhost:20002/postgres --inputs={}'
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+time echo "$files" | parallel 'python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:20003/postgres --inputs={}'
